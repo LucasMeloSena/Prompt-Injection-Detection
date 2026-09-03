e922fde999648337f09e693f4f7b1c7616825b5d
Lucas Sena <lucasmelosena@gmail.com>
2026-09-03 14:42:36 -0300

    fix: deploy settings

--- modal_app.py
+++ modal_app.py
@@ -1,17 +1,20 @@
 import modal
 
-image = modal.Image.debian_slim(python_version="3.11").pip_install_from_pyproject(
-  "pyproject.toml"
+image = (
+  modal.Image.debian_slim(python_version="3.11")
+  .pip_install_from_pyproject("pyproject.toml")
+  .add_local_python_source("pid")
 )
 
 app = modal.App("prompt-injection-detector", image=image)
 
 @app.function(
-    image=image, 
-    min_containers=1,
-    gpu="any"
+  image=image,
+  secrets=[modal.Secret.from_name("pid-secrets")],
+  min_containers=1,
+  gpu="any"
 )
 @modal.asgi_app()
 def fastapi_app():
-  from src.pid.api.main import app
+  from pid.api.main import app
   return app
